120f4940120b7a90a9816bf0e6cce170e1feef90
Pawcharz <pawel030724@gmail.com>
2024-10-29 17:46:31 +0100

    Add training notebook and model definition

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class TinyModel(torch.nn.Module):
+
+  def __init__(self):
+    super(TinyModel, self).__init__()
+
+    self.linear1 = nn.Linear(6, 16)
+    self.activation = nn.ReLU()
+    self.linear2 = nn.Linear(16, 1)
+    self.softmax = nn.Softmax()
+
+  def forward(self, x):
+    x = self.linear1(x)
+    x = self.activation(x)
+    x = self.linear2(x)
+    x = self.softmax(x)
+    return x
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
